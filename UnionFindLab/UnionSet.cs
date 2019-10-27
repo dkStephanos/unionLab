ba02066035df2f7f24dd2fc3849f68200e2f9729
dkStephanos <stephanos@goldmail.etsu.edu>
2019-10-26 23:00:51 -0400

    Added Kruskals aglorithm and refactor with print

--- UnionFindLab/UnionSet.cs
+++ UnionFindLab/UnionSet.cs
@@ -6,6 +6,30 @@ namespace UnionFindLab
 {
     class UnionSet
     {
+        public int numVertices, numEdges;
+        public Edge[] edges;
+        UnionItem[] unionItems;
+
+        public UnionSet(int numVertices, int numEdges) {
+            this.numVertices = numVertices;
+            this.numEdges = numEdges;
+            this.unionItems = new UnionItem[numVertices];        
+              
+            for (int i = 0; i < numVertices; ++i) 
+            { 
+                this.unionItems[i] = new UnionItem(); 
+                this.unionItems[i].parent = this.unionItems[i]; 
+                this.unionItems[i].value = i; 
+                this.unionItems[i].rank = 0; 
+            } 
+            edges = new Edge[numEdges];
+            for (int i = 0; i < numEdges; i++)
+			{
+                edges[i] = new Edge();
+			}
+        }
+    
+
         public void MakeSet(UnionItem x)
         {
             x.parent = x;
@@ -41,5 +65,41 @@ namespace UnionFindLab
         {
             Link(FindSet(x), FindSet(y));
         }
+
+        public void KruskalMST() 
+        { 
+            Edge[] MST = new Edge[numVertices - 1]; 
+            Edge nextEdge = new Edge(); 
+            int currentEdgeCount = 0; 
+            int currentEdgeIndex = 0;
+            
+            for (int i = 0; i < numVertices - 1; ++i) {
+                MST[i] = new Edge(); 
+            }
+            
+            Array.Sort(edges); 
+  
+            while (currentEdgeCount < numVertices - 1) 
+            {  
+                nextEdge = edges[currentEdgeIndex++]; 
+  
+                UnionItem x = FindSet(unionItems[nextEdge.source]); 
+                UnionItem y = FindSet(unionItems[nextEdge.destination]); 
+                
+                //Check for cycle
+                if (x != y) 
+                { 
+                    MST[currentEdgeCount++] = nextEdge; 
+                    Union(x, y); 
+                }   
+            }
+            edges = MST;
+        } 
+
+        public void PrintMST() {
+            Console.WriteLine("\n\nThe MST for the entered graph:\n"); 
+            for (int i = 0; i < edges.Length; ++i) 
+                    Console.WriteLine("Nodes: " + edges[i].source + " -- " + edges[i].destination + ", Weight = " + edges[i].weight); 
+        }                  
     }
 }
